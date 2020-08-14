599de852ad388877d967efc906bd93a198fb3971
Gayeon <gayeon0090990@gmail.com>
2020-08-13 19:28:10 -0700

    Multiply

---
+++ js.js
@@ -0,0 +1,5 @@
+function Multiply() {
+    var number1 = parseInt(prompt("Give me the first number"));
+    var number2 = parseInt(prompt("Give me the second number"));
+    document.getElementById("output").innerHTML = number1 * number2;
+}
